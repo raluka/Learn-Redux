40f62b863ae0515b26d3c3adfd2dac77a134ac59
raluka <badoi.raluca@gmail.com>
2018-08-19 16:31:30 +0200

    understanding reducer's job and dispatching actions 	every time you run fire an action, every reducer runs, and react or not on action type, depending on implemented logic

--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -1,10 +1,10 @@
 // a reducer takes 2 things:
 
 // 1. the action (info about what happened)
-
 // 2. copy of current state
 
 function posts(state = [], action) {
+  console.log('The post will change');
   console.log(state, action);
   return state;
 }

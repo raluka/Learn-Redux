16f3c0c86523f49ac494f3c381b2f57baab1395b
raluka <badoi.raluca@gmail.com>
2018-08-19 16:23:05 +0200

    integrate store with react router

--- learn-redux/client/reducers/index.js
+++ learn-redux/client/reducers/index.js
@@ -1,10 +1,10 @@
-import { combineREducers } from 'redux';
+import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
 import posts from './posts';
 import comments from './comments';
 
-// TODO: why we need router? what does it?
-const rootReducer = combineREducers( {posts, comments, router: routerReducer});
+// TODO: why we need routing? what does it?
+const rootReducer = combineReducers( {posts, comments, routing: routerReducer});
 
 export default rootReducer;
